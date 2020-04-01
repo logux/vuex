e71a3c47adaebc33f21c5702b4f122f4dcee8b69
Nikolay Govorov <nikolay_govorov@bk.ru>
2020-04-01 21:25:05 +0300

    Add the TS-typings

---
+++ create-logux/errors.ts
@@ -0,0 +1,26 @@
+import { createLogux } from '..'
+
+let opts = {
+  server: 'wss://localhost:1337',
+  subprotocol: '1.0.0',
+  userId: "10",
+}
+
+interface RootState {
+  value: number;
+}
+
+let Logux = createLogux(opts)
+let store = new Logux.Store<RootState>({
+  state: {
+    value: 0
+  },
+  mutations: {
+    increment(state: RootState): void {
+      state.value = state.value + 1
+    }
+  }
+})
+
+// THROWS Type 'number' is not assignable to type 'string[] | undefined'.
+store.commit.crossTab({ type: 'increment' }, { reasons: 1 })
---
+++ create-logux/index.d.ts
@@ -0,0 +1,133 @@
+import { Unsubscribe } from 'nanoevents'
+import { Action as ClientAction, Log } from "@logux/core";
+import { ClientMeta, ClientOptions, CrossTabClient } from "@logux/client";
+import { Payload as VuexPayload, Commit as VuexCommit, Store as VuexStore, CommitOptions } from "vuex";
+
+export type Action = ClientAction & VuexPayload;
+
+export interface Commit extends VuexCommit {
+  (type: string, payload?: any, options?: CommitOptions): void;
+  <A extends Action>(payloadWithType: A, options?: CommitOptions): void;
+
+  /**
+   * Add sync action to log and update store state.
+   * This action will be visible only for server and all browser tabs.
+   *
+   * ```js
+   * store.commit.sync(
+   *   { type: 'CHANGE_NAME', name },
+   *   { reasons: ['lastName'] }
+   * ).then(meta => {
+   *   store.log.removeReason('lastName', { maxAdded: meta.added - 1 })
+   * })
+   * ```
+   *
+   * @param action The new action.
+   * @param meta Action’s metadata.
+   * @returns Promise when action will be processed by the server.
+   */
+  sync<A extends Action>(action: A, meta?: Partial<ClientMeta>): Promise<ClientMeta>;
+
+  /**
+   * Add cross-tab action to log and update store state.
+   * This action will be visible only for all tabs.
+   *
+   * ```js
+   * store.commit.crossTab(
+   *   { type: 'CHANGE_FAVICON', favicon },
+   *   { reasons: ['lastFavicon'] }
+   * ).then(meta => {
+   *   store.log.removeReason('lastFavicon', { maxAdded: meta.added - 1 })
+   * })
+   * ```
+   *
+   * @param action The new action.
+   * @param meta Action’s metadata.
+   * @returns Promise when action will be saved to the log.
+   */
+  crossTab<A extends Action>(action: A, meta?: Partial<ClientMeta>): Promise<ClientMeta>;
+
+  /**
+   * Add local action to log and update store state.
+   * This action will be visible only for current tab.
+   *
+   * ```js
+   *
+   * store.commit.local(
+   *   { type: 'OPEN_MENU' },
+   *   { reasons: ['lastMenu'] }
+   * ).then(meta => {
+   *   store.log.removeReason('lastMenu', { maxAdded: meta.added - 1 })
+   * })
+   * ```
+   *
+   * @param action The new action.
+   * @param meta Action’s metadata.
+   * @returns Promise when action will be saved to the log.
+   */
+  local<A extends Action>(action: A, meta?: Partial<ClientMeta>): Promise<ClientMeta>;
+}
+
+interface StateListener<S> {
+  <A extends Action>(state: S, prevState: S, action: A, meta: ClientMeta): void
+}
+
+export declare class Store<S = any> extends VuexStore<S> {
+  /**
+   * Add action to log with Vuex compatible API.
+   */
+  commit: Commit
+
+  /**
+   * Subscribe for store events.
+   *
+   * ```js
+   * store.on('change', (state, prevState, action, meta) => {
+   *   console.log(state, prevState, action, meta)
+   * })
+   * ```
+   *
+   * @param event The event name.
+   * @param listener The listener function.
+   * @returns Unbind listener from event.
+   */
+  on(event: 'change', listener: StateListener<S>): Unsubscribe
+
+  /**
+   * Logux synchronization client.
+   */
+  client: CrossTabClient
+
+  /**
+   * The Logux log.
+   */
+  log: Log<ClientMeta>
+}
+
+export type LoguxConfig = ClientOptions & {
+  /**
+   * How often we need to clean log from old actions. Default is every `25`
+   * actions.
+   */
+  cleanEvery?: number
+
+  /**
+   * How many actions without `meta.reasons` will be kept for time travel.
+   * Default is `1000`.
+   */
+  reasonlessHistory?: number;
+
+  /**
+   * How often save state to history. Default is `50`.
+   */
+  saveStateEvery?: number;
+
+  /**
+   * Callback when there is no history to replay actions accurate.
+   */
+  onMissedHistory?: (action: ClientAction) => void;
+}
+
+export function createLogux(config: LoguxConfig): {
+  Store: typeof Store
+};
