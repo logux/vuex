5c4bd4ee0a46b575c6c8d1d28c06327821ab5148
Eduard Aksamitov <e@edaks.co>
2020-04-07 15:06:00 +0300

    types(mixin): add channels in positive test

--- subscription-mixin/types.ts
+++ subscription-mixin/types.ts
@@ -3,6 +3,14 @@ import mixins from 'vue-typed-mixins';
 import { subscriptionMixin } from "./..";
 
 mixins(subscriptionMixin).extend({
+  computed: {
+    channels () {
+      return [
+        'user',
+        { channel: `user/1`, fields: ['name'] }
+      ]
+    }
+  },
   mounted() {
     console.log(this.isSubscribing);
   }
