32d9c34945cf1acc67d8086ae4faec5a1c21977c
Nikolay Govorov <nikolay_govorov@bk.ru>
2020-04-10 00:17:08 +0300

    Add the `vue-class-component` example for subscriptionMixin

--- subscription-mixin/errors.ts
+++ subscription-mixin/errors.ts
@@ -1,12 +1,16 @@
-import mixins from 'vue-typed-mixins'
+import Component, { mixins } from 'vue-class-component'
 
 import { subscriptionMixin } from '..'
 
-mixins(subscriptionMixin).extend({
+@Component
+class UserPhoto extends mixins(subscriptionMixin) {
   mounted() {
     // THROWS Type 'boolean' is not assignable to type 'string'.
     let message: string = this.isSubscribing
 
     console.log(message)
   }
-})
+}
+
+let photo = new UserPhoto()
+console.log(photo);
