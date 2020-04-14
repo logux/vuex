fd7896b4917379e451e54aeb138024e648dde724
Eduard Aksamitov <e@edaks.co>
2020-04-14 17:44:27 +0300

    docs(ts): add subscriptionMixin

--- subscription-mixin/index.d.ts
+++ subscription-mixin/index.d.ts
@@ -9,9 +9,50 @@ export interface SubscriptionComponent {
   channels: Channel[]
 }
 
+/**
+ * Mixin, which takes care of subscribtions and unsubscribtions
+ * in your component during the life cycle.
+ *
+ * It watches for `channels` changes
+ * and `isSubscribing` indicates loading state.
+ *
+ * ```html
+ * <template>
+ *   <div v-if="isSubscribing">
+ *     <h1>Loading</h1>
+ *   </div>
+ *   <div v-else>
+ *     <h1>{{ user.name }}</h1>
+ *   </div>
+ * </template>
+ *
+ * <script>
+ * import { subscriptionMixin } from '@logux/vuex'
+ *
+ * export default {
+ *   name: 'UserProfile',
+ *   mixins: [subscriptionMixin],
+ *   props: {
+ *     userId: String
+ *   },
+ *   computed: {
+ *     user () {
+ *       return this.$store.state.user
+ *     },
+ *     channels () {
+ *       return [`user/${ userId }`]
+ *     }
+ *   }
+ * }
+ * </script>
+ * ```
+ */
 export const subscriptionMixin: ExtendedVue<
   Vue,
   {
+    /**
+     * Indicates loading state.
+     */
     isSubscribing: boolean,
     $_loguxVuex_ignoreResponse: {
       [id: string]: boolean
