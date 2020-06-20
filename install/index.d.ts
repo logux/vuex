cb417e5975ff1f1b27adbf4a910726f10eeb1679
Eduard Aksamitov <e@edaks.co>
2020-06-20 16:28:29 +0300

    docs(ts): cleanup

--- install/index.d.ts
+++ install/index.d.ts
@@ -4,9 +4,9 @@ import _Vue from 'vue'
  * When used with a module system,
  * you must explicitly install Logux Vuex via `Vue.use()`
  *
- * ```
+ * ```js
  * import Vue from 'vue'
- * import { LoguxVuex } from 'logux/vuex'
+ * import { LoguxVuex } from '@logux/vuex'
  *
  * Vue.use(LoguxVuex)
  * ```
