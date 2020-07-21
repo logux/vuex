f60cf1cee28a813adb17457a7ed7fd4ab3d3cd8e
Eduard Aksamitov <e@edaks.co>
2020-07-21 23:57:54 +0300

    chore(component): update with use composable function

--- component/index.js
+++ component/index.js
@@ -1,81 +1,27 @@
-const {
-  subscribe,
-  unsubscribe,
-  subscriptionsId,
-  unifyChannelsObject
-} = require('../helpers')
+let { toRefs } = require('vue')
+
+let { useSubscription } = require('../composable')
 
 let loguxComponent = {
+  name: 'LoguxComponent',
   props: {
     channels: {
       type: Array,
       required: true
-    },
-    tag: {
-      type: String,
-      default: 'div'
     }
   },
-  data: () => ({
-    isSubscribing: false,
-    ignoreResponse: {}
-  }),
-  watch: {
-    channels: {
-      handler (newChannels, oldChannels) {
-        let newSubscriptions = unifyChannelsObject(newChannels)
-        let oldSubscriptions = unifyChannelsObject(oldChannels)
+  setup (props, { slots }) {
+    let { channels } = toRefs(props)
 
-        let newId = subscriptionsId(newSubscriptions)
-        let oldId = subscriptionsId(oldSubscriptions)
-
-        if (newId !== oldId) {
-          this.subscribe(newSubscriptions)
-          oldChannels && this.unsubscribe(oldSubscriptions)
-        }
-      },
-      immediate: true
-    }
-  },
-  beforeDestroy () {
-    let subscriptions = unifyChannelsObject(this.channels)
-    this.unsubscribe(subscriptions)
-  },
-  methods: {
-    async subscribe (subscriptions) {
-      this.isSubscribing = true
-
-      let id = subscriptionsId(subscriptions)
-      delete this.ignoreResponse[id]
-
-      await subscribe(this.$store, subscriptions)
-      if (!this.ignoreResponse[id]) {
-        this.isSubscribing = false
-      }
-    },
-    unsubscribe (subscriptions) {
-      let id = subscriptionsId(subscriptions)
-      this.ignoreResponse[id] = true
-
-      unsubscribe(this.$store, subscriptions)
-    }
-  },
-  render (h) {
-    let { isSubscribing } = this
-    let defaultSlot = this.$scopedSlots.default
+    let defaultSlot = slots.default
 
     if (!defaultSlot) {
       throw new Error('Provided scoped slot is empty')
     }
 
-    let vnode = defaultSlot({ isSubscribing })
+    let isSubscribing = useSubscription(channels)
 
-    // for arrays and single text nodes
-    if (vnode && (vnode.length > 1 || !vnode[0].tag)) {
-      return h(this.tag, {}, vnode)
-    }
-
-    return vnode
+    return () => defaultSlot({ isSubscribing })
   }
 }
 
