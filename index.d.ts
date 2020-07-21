237eb101302c8436af2aff99a9ff35fce9ac2cb2
Eduard Aksamitov <e@edaks.co>
2020-07-21 23:54:32 +0300

    feat(composable): init

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
 export { loguxMixin, Channel, Subscription, loguxMixinComponent } from './mixin'
+export { useSubscription, Channels } from './composable'
 export { loguxComponent } from './component'
 export { createLogux } from './create-logux'
 export { LoguxVuex } from './install'
--- index.js
+++ index.js
@@ -2,10 +2,12 @@ let { createLogux } = require('./create-logux')
 let { LoguxVuex } = require('./install')
 let { loguxMixin } = require('./mixin')
 let { loguxComponent } = require('./component')
+let { useSubscription } = require('./composable')
 
 module.exports = {
   createLogux,
   LoguxVuex,
   loguxMixin,
-  loguxComponent
+  loguxComponent,
+  useSubscription
 }
