d082f68acb1122abfc55c6f2253920e43b4293e9
Eduard Aksamitov <e@edaks.co>
2020-07-22 16:05:16 +0300

    docs(ts): add composable example and description

--- composable/index.d.ts
+++ composable/index.d.ts
@@ -4,7 +4,39 @@ import { Channel } from '..'
 
 export type Channels = ComputedRef<Channel[]> | Channel[]
 
-/*
+/**
+ * Composable function to subscribe for channels during component render
+ * and unsubscribe on component unmount.
+ *
+ * ```html
+ * <template>
+ *   <div v-if="isSubscribing">Loading</div>
+ *   <h1 v-else>{{ user.name }}</h1>
+ * </template>
+ *
+ * <script>
+ * import { toRefs } from 'vue
+ * import { useStore, useSubscription } from '@logux/vuex'
+ *
+ * export default {
+ *   props: {
+ *     userId: String
+ *   },
+ *   setup (props) {
+ *     let store = useStore()
+ *     let { userId } = toRefs(props)
+ *
+ *     let channels = computed(() => [`user/${userId}`])
+ *     let isSubscribing = useSubscription(channels)
+ *
+ *     let user = computed(() => store.state.users[userId])
+ *
+ *     return { isSubscribing, user }
+ *   }
+ * })
+ * </script>
+ *
+ * ```
  * @param channels Channels to subscribe.
  * @return `true` during data loading.
  */
