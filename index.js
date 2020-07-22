7826834bf5637e5faea756f88b91c1e43c7d1d69
Eduard Aksamitov <e@edaks.co>
2020-07-22 15:55:23 +0300

    feat: export vuex useStore

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+let { useStore } = require('vuex')
+
 let { createLogux } = require('./create-logux')
 // let { loguxMixin } = require('./mixin')
 let { loguxComponent } = require('./component')
 let { useSubscription } = require('./composable')
 
 module.exports = {
+  useStore,
   createLogux,
   // loguxMixin,
   loguxComponent,
