32ece539571b8103f0bf51f2b72082a13fff0375
Eduard Aksamitov <e@edaks.co>
2020-07-22 04:09:21 +0300

    chore(utils): cleanup

--- utils/isPromise/index.test.js
+++
@@ -1,9 +0,0 @@
-let { isPromise } = require('.')
-
-it('isPromise', () => {
-  let promise = new Promise(() => {}, () => {})
-  let func = () => {}
-  expect(isPromise(1)).toBe(false)
-  expect(isPromise(promise)).toBe(true)
-  expect(isPromise(func)).toBe(false)
-})
