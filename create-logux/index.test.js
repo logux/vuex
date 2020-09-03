e6f0898246080085033dd0eab072c3172eaedbfb
Eduard Aksamitov <e@edaks.co>
2020-09-03 21:16:39 +0300

    test(store): improve “undoes actions”

--- create-logux/index.test.js
+++ create-logux/index.test.js
@@ -329,23 +329,37 @@ it('changes history', async () => {
 })
 
 it('undoes actions', async () => {
-  let store = createStore({ historyLine })
+  let store = createStore({ historyLine }, {
+    saveStateEvery: 1
+  })
   let nodeId = store.client.nodeId
 
   await Promise.all([
-    store.crossTab(
+    store.commit.crossTab(
       { type: 'historyLine', value: 'a' }, { reasons: ['test'] }),
-    store.crossTab(
+    store.commit.crossTab(
       { type: 'historyLine', value: 'b' }, { reasons: ['test'] }),
-    store.crossTab(
+    store.commit.crossTab(
       { type: 'historyLine', value: 'c' }, { reasons: ['test'] })
   ])
   expect(store.state.value).toEqual('0abc')
-  store.crossTab(
-    { type: 'logux/undo', id: `2 ${nodeId} 0` }, { reasons: ['test'] }
+
+  await store.commit.crossTab(
+    { type: 'logux/undo', id: `3 ${nodeId} 0` }, { reasons: ['test'] }
   )
   await delay(1)
-  expect(store.state.value).toEqual('0ac')
+  expect(store.state.value).toEqual('0ab')
+
+  await store.commit.crossTab(
+    { type: 'historyLine', value: 'd' }, { reasons: ['test'] }
+  )
+  expect(store.state.value).toEqual('0abd')
+
+  await store.commit.crossTab(
+    { type: 'logux/undo', id: `5 ${nodeId} 0` }, { reasons: ['test'] }
+  )
+  await delay(1)
+  expect(store.state.value).toEqual('0ab')
 })
 
 it('ignores cleaned history from non-legacy actions', async () => {
